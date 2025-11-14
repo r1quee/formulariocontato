b7b99b63179fde68ea2cd4634ed3c90398965156
r1quee <henrique.evangelista08@gmail.com>
2025-11-14 15:28:18 +0000

    A

--- app/routes/router.js
+++ app/routes/router.js
@@ -2,38 +2,55 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: "henrique.evangelista08@gmail.com",
-        pass: "ompe yhon jwjh heej"
-    }
+router.get("/", (req, res) => {
+  res.render("pages/formulariocontato");
 });
 
-const mailOptions = {
-    from: "henrique.evangelista08@gmail.com",
-    to: "ggamer0808@gmail.com",
-    subject: "assunto",
-    text: "mensagem em formato de texto",
-    html: "<h1>mensagem em formato HTML</h1>"
-}
+router.post("/formulariocontato", (req, res) => {
+  // NODEMAILER
+  const { nome, mensagem, email, telefone , assunto } = req.body;
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
 
-transporter .sendMail(mailOptions, (error, info) => {
-    if(error){
-        console.log(error);
-    } else{
-        console.log(info);
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
+        subject: `${assunto}`,
+        
+        html: `
+        <h2>Novo envio do formulário</h2>
+        <p><b>Nome:</b> ${nome}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Telefone:</b> ${telefone}</p>
+        <p><b>Mensagem:</b> ${mensagem}</p>
+      `,
+  };
+        
+    
 
-        console.log("email enviado");
+     transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Erro:", error);
+      return res.send("Erro ao enviar e-mail");
     }
-})
 
-
-router.get("/", (req, res)=>{
-    res.render("pages/formulariocontato", {resultados:null,valores:{numero:""}});
+    console.log("Email enviado:", info.response);
+    res.send("E-mail enviado com sucesso!");
+    });
 });
+
+module.exports = router;
+
+
+
+    
 
 
 
